26a3714326ccfc21ce33bbd7adf62d3f87b3b5d1
caizhen <zhenchai0000@gmail.com>
2024-04-02 20:16:08 +0800

    update: love asm to display love symbol with red color

--- week6/love.asm
+++ week6/love.asm
@@ -2,34 +2,34 @@
 .stack
 .code
 
-; bios services
 Main Proc
 
-    mov ah, 02h ; can also write 02 ; call function to display
-    mov dl, 73  ; display value ;a;
+    mov ah, 02h
+    mov dl, 73
     int 21h
 
     mov ah, 02h
-    mov al, 3 
-    ; mov bh, 0
-    ; mov bl, 12  ; change text code
+    mov dl, ' '
     int 21h
 
-    mov ah, 02h ; can also write 02 ; call function to display
-    mov dl, 85  ; display value ;a;
+    mov ah, 09h
+    mov bl, 4
+    mov cx, 1
+    int 10h
+
+    mov ah, 02h
+    mov dl, 3 
     int 21h
 
-    ; mov al, 65h
-    ; mov bh, 0
-    ; mov bl, 10  ; change text code
-    ; int 10h
+    mov ah, 02h
+    mov dl, ' '
+    int 21h
 
-    ; mov al, 85
-    ; mov bh, 0
-    ; mov bl, 10  ; change text code
-    ; int 10h
+    mov ah, 02h
+    mov dl, 85
+    int 21h
 
     mov ah, 4ch
     int 21h
 Main endp
-end main
+end Main
