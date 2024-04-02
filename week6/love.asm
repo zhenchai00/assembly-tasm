464c305f44f298d3720f208671006f7cff5037dd
caizhen <zhenchai0000@gmail.com>
2024-04-02 20:17:35 +0800

    update: add comment

--- week6/love.asm
+++ week6/love.asm
@@ -3,28 +3,33 @@
 .code
 
 Main Proc
-
+    ; Function to display 'I'
     mov ah, 02h
     mov dl, 73
     int 21h
 
+    ; Function to display ' '
     mov ah, 02h
     mov dl, ' '
     int 21h
 
+    ; Function to display text color red by using BIOS services
     mov ah, 09h
     mov bl, 4
     mov cx, 1
     int 10h
 
+    ; Function to display '♥'
     mov ah, 02h
     mov dl, 3 
     int 21h
 
+    ; Function to display ' '
     mov ah, 02h
     mov dl, ' '
     int 21h
 
+    ; Function to display 'U'
     mov ah, 02h
     mov dl, 85
     int 21h
